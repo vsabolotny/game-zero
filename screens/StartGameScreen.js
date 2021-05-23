55a2c84a2b94b453717a2c7b68d0fa1460cae07a
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-23 21:37:55 +0200

    Modify number container with nice output

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import {
 import Card from "../components/Card";
 import Colors from "../constants/Colors";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 
 const StartGameScreen = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -40,12 +41,19 @@ const StartGameScreen = (props) => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue("");
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" />
+      </Card>
+    );
   }
 
   return (
@@ -118,6 +126,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center"
   },
 });
 
